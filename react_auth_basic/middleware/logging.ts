6e3fbd5c8a4c6d482eaa4828feeaaea8ad716087
中島浩二 <knaka0209@gmail.com>
2025-03-25 12:30:18 +0900

    up, 0325a3

---
+++ react_auth_basic/middleware/logging.ts
@@ -0,0 +1,8 @@
+import { getRequestURL, sendRedirect } from 'h3';
+import LibConfig from "@/src/lib/LibConfig";
+
+export default defineEventHandler((event) => {
+  // Extends or modify the event
+  //console.log("#log");
+  //event.context.user = { name: 'Nitro' }
+})
